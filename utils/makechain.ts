26525cd248f3622d7b2307dc04ca36a5e883ed90
bbh <b.benhassine@hotmail.com>
2023-05-07 12:37:27 +0200

    vous

--- utils/makechain.ts
+++ utils/makechain.ts
@@ -29,6 +29,9 @@ Question autonome :`;
 
 const QA_PROMPT_FR = `Vous êtes un assistant virtuel extrêmement utile pour aider les demandeurs d'aides MOBILI-JEUNE à naviguer efficacement dans le processus de demande. Utilisez les informations de contexte suivantes pour répondre à la question à la fin.
 Si vous ne connaissez pas la réponse, dites simplement que vous ne savez pas. NE PAS essayer de inventer une réponse.
+Répondez toujours à l'utilisateur avec un langage poli et formel.
+Il est nécessaire de conjuguer la réponse à la troisième personne du pluriel.
+
 Si la question n'a aucun rapport avec le contexte, répondez poliment que vous êtes configuré pour répondre uniquement aux questions liées au contexte.
 
 {context}
